ffacfee43e4284649048068c8db46fc24ebfbd02
eogasawara <eogasawara@eic.cefet-rj.br>
2024-06-23 19:20:02 -0300

    update

--- R/data.R
+++ R/data.R
@@ -14,21 +14,3 @@
 #' data(st_drift_examples)
 #' dataset <- st_drift_examples$example1
 "st_drift_examples"
-
-#' Real time series for concept drift detection
-#' @description A list of real multivariate time series for concept drift detection
-#' \itemize{
-#' \item bfd1: Brazilian Flights Data 2023
-#' }
-#'#'
-#' @docType data
-#' @usage data(st_real_examples)
-#' @format A list of real multivariate time series.
-#' @keywords datasets
-#' @references \href{https://github.com/cefet-rj-dal/heimdall}{Stealthy package}
-#' @source \href{https://github.com/cefet-rj-dal/heimdall}{Stealthy package}
-#' @examples
-#' data(st_real_examples)
-#' dataset <- st_real_examples$bfd1
-"st_real_examples"
-
