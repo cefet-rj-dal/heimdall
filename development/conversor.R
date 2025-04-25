ddd4d118c290e8d07d0b9366d0eb420df1c18b86
eogasawara@ieee.org <eogasawara@ieee.org>
2025-04-24 22:47:24 -0300

    update

--- development/conversor.R
+++ development/conversor.R
@@ -1,10 +1,10 @@
 convert_ipynb_to_rmarkdown <- function(input) {
   if (!require("rmarkdown")) return("Missing necessary package: 'rmarkdown'")
-
+  
   if (tolower(xfun::file_ext(input)) != "ipynb") {
     return( "Error: Invalid file format" )
   }
-
+  
   rmarkdown::convert_ipynb(input)
 }
 
